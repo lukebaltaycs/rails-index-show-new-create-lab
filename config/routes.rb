63a37cc0165f20ecbac7ebf3e92c6966318519fb
Luke Baltay <luke.baltay@yale.edu>
2022-06-24 13:48:18 -0400

    commit tree eb520d73a3f0d6fc93c465386cbc3ae052f30ab6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :coupons
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get 'coupon/:id', to: 'coupons#view', as: 'student'
+  get 'coupons', to: 'coupons#index', as: 'students'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
